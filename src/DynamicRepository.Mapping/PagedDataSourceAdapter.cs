5610ad5524762827dcccfbf71dcb80f7a4be4f39
Rafael Carvalho <rpaschoal@live.com>
2016-08-04 17:50:23 +1200

    Initial implementation of "DynamicRepository.Mapping" project. Implemented major Attribute classes.

---
+++ src/DynamicRepository.Mapping/PagedDataSourceAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicRepository.Mapping
+{
+    /// <summary>
+    /// Translates API public filter settings to possible back-end properties.
+    /// </summary>
+    /// <remarks>
+    /// By using these adapters it helps building an API that is less prompt to cause UI to break,
+    /// since after it Entity properties can be renamed without affecting how UI name or interface with them.
+    /// </remarks>
+    public static class PagedDataSourceAdapter
+    {
+    }
+}
---
+++ src/DynamicRepository.Mapping/PagedDataSourceDefaultSortingAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicRepository.Mapping
+{
+    /// <summary>
+    /// Use this on API endpoint or MVC controller to configure default sorting property for paged data source results.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class PagedDataSourceDefaultSortingAttribute : Attribute
+    {
+        public PagedDataSourceDefaultSortingAttribute(string property, bool isAscending = false)
+        {
+            this.Property = property;
+            this.IsAscending = isAscending;
+        }
+
+        /// <summary>
+        /// The Entity property which will be used as default sorting.
+        /// </summary>
+        public string Property { get; set; }
+
+        /// <summary>
+        /// If true, means it will be sorted by ascending.
+        /// </summary>
+        /// <remarks>
+        /// Default sorting order is Descending.
+        /// </remarks>
+        public bool IsAscending { get; set; }
+    }
+}
