e8ed38825ae737f500bf8affa07f4dc0b89ae7d9
Rafael Carvalho <rpaschoal@live.com>
2016-10-07 20:28:40 -0300

    Created a unit tests project. Implemented "CanGetPagedData" unit test.

---
+++ src/DynamicRepository.Tests/DataPagerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using DynamicRepository.Tests.Support;
+using DynamicRepository;
+using Xunit;
+using DynamicRepository.Filter;
+using System.Linq;
+
+namespace DynamicRepository.Tests
+{
+    /// <summary>
+    /// Tests basic behaviors of the Data Pager class.
+    /// </summary>
+    public class DataPagerTests
+    {
+        protected DataPager<long, MockModel> _dataPager { get; set; } = new DataPager<long, MockModel>();
+
+        public DataPagerTests()
+        {
+        }
+
+        /// <summary>
+        /// Evaluates if data can be paged over an IQueryable instance.
+        /// </summary>
+        [Fact]
+        public void CanGetPagedData()
+        {
+            // Arrange
+            var items = MockModel.ArrangeFixture();
+            var pagedSize = 5;
+
+            var settings = new PagedDataSettings()
+            {
+                TotalPerPage = pagedSize
+            };
+
+            // Act
+            var result = _dataPager.GetPagedData(items.AsQueryable(), settings);
+
+            // Assert
+            Assert.True(result.TotalRecords == items.Count, "Total records in the paged result does not match the total in the fixture collection.");
+            Assert.True(result.Result.Count == pagedSize, "Total items in first paged batch does not match the configured page size.");
+        }
+    }
+}
---
+++ src/DynamicRepository.Tests/Support/MockModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicRepository.Tests.Support
+{
+    /// <summary>
+    /// Simple POCO to be used under this test project.
+    /// </summary>
+    public sealed class MockModel
+    {
+        public long Id { get; set; }
+        public string Label { get; set; }
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Provides a collection of data to be used in arrange phase of unit tests.
+        /// </summary>
+        /// <remarks>
+        /// TODO: Integrate with AutoFixture project for future test growth.
+        /// </remarks>
+        /// <returns>
+        /// Collection of fixtured data for tests.
+        /// </returns>
+        public static IList<MockModel> ArrangeFixture()
+        {
+            return new List<MockModel>()
+            {
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "First Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Second Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Third Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Fourth Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Fifth Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Sixth Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Seventh Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Eigth Label"
+                },
+                new MockModel()
+                {
+                    Id = 1,
+                    Label = "Nineth Label"
+                }
+            };
+        }
+    }
+}
