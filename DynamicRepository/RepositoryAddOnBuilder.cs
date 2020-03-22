2077527906e851b56f117a4ec92779396d2ee019
Rafael Carvalho <rpaschoal@live.com>
2020-03-22 13:54:28 +1300

    Add RepositoryAddOnBuilder class

---
+++ DynamicRepository/RepositoryAddOnBuilder.cs
@@ -0,0 +1,33 @@
+﻿using DynamicRepository.Resiliency;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicRepository
+{
+    public class RepositoryAddOnBuilder<Key, Entity> where Entity : class
+    {
+        private IRepository<Key, Entity> _repositoryBuild;
+
+        public RepositoryAddOnBuilder(IRepository<Key, Entity> providerRepositoryInstance)
+        {
+            _repositoryBuild = providerRepositoryInstance;
+        }
+
+        public RepositoryAddOnBuilder<Key, Entity> AddResiliency()
+        {
+            if (_repositoryBuild == null)
+            {
+                throw new Exception("No repository instance to add resiliency for the current DynamicRepository being built.");
+            }
+
+            _repositoryBuild = new ResilientRepositoryDecorator<Key, Entity>(_repositoryBuild);
+            return this;
+        }
+
+        public IRepository<Key, Entity> Build()
+        {
+            return _repositoryBuild;
+        }
+    }
+}
