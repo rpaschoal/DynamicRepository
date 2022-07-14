a425cafec4970f5279041753564d506f2cd383bd
Rafael Carvalho <rpaschoal@live.com>
2022-07-14 16:14:59 +1200

    Add Ambient transaction register

---
+++ DynamicRepository.MongoDB/TransactionRegister.cs
@@ -0,0 +1,10 @@
+﻿using DynamicRepository.Transaction;
+using System.Collections.Concurrent;
+
+namespace DynamicRepository.MongoDB
+{
+    internal static class TransactionRegister
+    {
+        internal static ConcurrentDictionary<string, ITransaction> AmbientTransactions = new ConcurrentDictionary<string, ITransaction>();
+    }
+}
